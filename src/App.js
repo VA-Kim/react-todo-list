b7ac0c2e8d54bdf3f1558ef99ef92c5059af72c3
VA-Kim <icewkd@gmail.com>
2019-05-11 00:46:53 +0900

    write init data & rendering

--- src/App.js
+++ src/App.js
@@ -4,11 +4,31 @@ import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
 
 class App extends Component {
+  state = {
+    input: '', // input 값
+    todos: [
+      { id:0, text: '용산IMAX-어벤져스 예매하기', done: true},
+      { id:1, text: '내일이 발표인데...', done: false}
+    ]
+  }
+
+  handleChange = (e) => {
+    const { value } = e.target;
+    this.setState({
+      input: value
+    });
+  }
+
   render() {
+    const { input, todos } = this.state;
+    const { 
+      handleChange
+    } = this;
+
     return (
       <PageTemplate>
-        <TodoInput/>
-        <TodoList/>
+        <TodoInput onChange={handleChange} value={input}/>
+        <TodoList todos={todos}/>
       </PageTemplate>
     );
   }
